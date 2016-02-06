976aabc5759a46d049a65ab27a06f6b9b4c56a04
JimC <jimc1664@outlook.com>
2016-02-06 22:59:05 +0000

    a

---
+++ Assets/Scripts/Carrier.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Networking;
+
+public class Carrier : Unit {
+
+    public GameObject Unit1;
+    public GameObject Unit2;
+
+    public List<GameObject> SpawnPoints;
+
+    float BuildTimer = 1;
+    void Update() {
+        BuildTimer -= Time.deltaTime;
+        if( BuildTimer > 0 ) return;
+        if(Input.GetKeyUp( KeyCode.Alpha1 )) {
+            createFrom(Unit1);
+            BuildTimer = 0.5f;
+        }
+        if(Input.GetKeyUp(KeyCode.Alpha2)) {
+            createFrom(Unit2);
+            BuildTimer = 1.0f;
+        }
+
+    }
+
+    void createFrom(GameObject fab) {
+
+
+        GameObject c = (GameObject)Instantiate(fab, Vector3.zero, Quaternion.identity);
+
+
+        NetworkServer.Spawn(c);
+
+        //todo -- SpawnPoints.Count  >= 256 == err
+        c.GetComponent<UnitSpawn_Hlpr>().Rpc_init( gameObject, (byte)Random.Range(0, SpawnPoints.Count));
+
+
+    }
+
+
+}
---
+++ Assets/Scripts/UnitSpawn_Hlpr.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+
+//todo.. requires unit... forgot sysntax and intellisense is pooped
+public class UnitSpawn_Hlpr : NetworkBehaviour {
+
+    Transform Prev, Next;
+    float D;
+    Unit U;
+    void Awake() {
+        U = GetComponent<Unit>();
+        U.enabled = false;
+        U.VisDat.SetActive(false);
+
+
+        foreach( Collider2D c in GetComponentsInChildren<Collider2D>())
+            c.enabled = false;
+
+      //  U.Body.
+    //    enabled = false;
+      //  gameObject.SetActive(false);
+    }
+
+    float Timer = 0;
+
+
+    [ClientRpc]
+    public void Rpc_init(GameObject co, byte spI) {
+
+        var c = co.GetComponent<Carrier>();
+        if(c == null || (uint)spI >= (uint)c.SpawnPoints.Count) {  //note: in network functions we are unduly careful
+            Destroy(gameObject);
+            Debug.LogError("Error in UnitSpawn_Hlpr::init  " + c + "  ---- " + (int)spI);
+            return;
+        }
+
+        Player o = c.Owner;
+        U.init(o);
+
+        var sp = c.SpawnPoints[spI];
+        Prev = sp.transform;
+
+        if(Prev.childCount > 0) Next = Prev.GetChild(0);
+       // U.Trnsfrm.parent = c.Trnsfrm;
+        U.Trnsfrm.position = Prev.position;
+        U.Trnsfrm.rotation = Prev.rotation;
+
+        Timer = 1;
+        //enabled = true;
+        // gameObject.SetActive(true);
+        U.VisDat.SetActive(true);
+
+        foreach(Collider2D c2 in GetComponentsInChildren<Collider2D>()) {
+            c2.enabled = true;
+        }
+
+        var j = gameObject.AddComponent<DistanceJoint2D>();  //disble inter collision by magic
+        j.connectedBody = c.Body;
+        j.maxDistanceOnly = true;
+        j.distance = 99999.0f;
+    }
+
+    void Update() {
+
+        if(Timer <= 0) {  //created but not init-ed
+            if((Timer -= Time.deltaTime) < -3) {
+                Destroy( gameObject);               
+            }
+            return;
+        }
+        
+        if( Next != null) {
+            float spd = U.MaxSpeed / 2;
+            float dis = (Next.position - Prev.position).magnitude;
+
+            D += spd * Time.deltaTime / dis;
+            if(D > 1.0f) {
+                Prev = Next;
+                if(Prev.childCount > 0) {
+                    Next = Prev.GetChild(0);
+                    D -= 1;
+                } else {
+                    Next = null;
+                    U.Trnsfrm.position = Prev.position;
+                }                
+            } 
+        }
+        if(Next != null) {
+            var p = Vector3.Lerp(Prev.position, Next.position, D);
+            U.Body.MovePosition(p);
+            U.Body.MoveRotation( Quaternion.Lerp(Prev.rotation, Next.rotation, D).eulerAngles.z );
+        } else { // our carrier == kaboom-boom  or just done..
+            U.enabled = true;
+            Destroy(GetComponent<DistanceJoint2D>());
+            Destroy(this);          
+            return;
+        } 
+
+    }
+}
