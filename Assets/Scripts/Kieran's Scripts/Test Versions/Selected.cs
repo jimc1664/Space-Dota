cd32cbbcad95f85da1232f4bb971a5d54f6f6824
kbrawley95 <kieran.brawley95@gmail.com>
2016-02-16 19:12:05 +0000

    Updated Selection Script (For Network Implementation)
    The player can now click selected units.
    
    Setup available in "BoxSelectionTest" scene.

---
+++ Assets/Scripts/Kieran's Scripts/Test Versions/Selected.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Selected : MonoBehaviour {
+
+    public bool selected = false;
+    private bool selectedByClick = false;
+
+    [SerializeField]
+    int counter = 0;
+
+    // Update is called once per frame
+    void Update () {
+
+        if (GetComponent<Renderer>().isVisible && Input.GetMouseButton(0))
+        {
+            if (!selectedByClick)
+            {
+                Vector3 camPos = Camera.main.WorldToScreenPoint(transform.position); //Transposes 3D Vector into 2D Screen Space (Unproject)
+                camPos.y = BoxSelector.ScreenToRectSpace(camPos.y);
+                selected = BoxSelector.selection.Contains(camPos);
+            }
+            
+        }
+
+        if (selected)
+            GetComponentInChildren<Projector>().enabled = true;
+        else
+            GetComponentInChildren<Projector>().enabled = false;
+
+        if (counter == 2)
+        {
+            selected = false;
+            selectedByClick = false;
+            counter = 0;
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        counter++;
+        selected = true;
+        selectedByClick = true;
+    }
+
+    private void OnMouseUp()
+    {
+        if (selectedByClick)
+            selected = true;
+    }
+}
