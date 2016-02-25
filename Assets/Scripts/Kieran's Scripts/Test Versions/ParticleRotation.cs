e00ca66036f100329d0997907687897ca5db5ad5
kbrawley95 <kieran.brawley95@gmail.com>
2016-02-25 18:50:21 +0000

    Further Selection Updates (Selected Al)
    The following functionality has been implemented via the Selection Test
    Scene:
    
    -All units can now be selected through the use of the (Left-Alt) Key.
    *Subject to change.
    -Setup for specified unit selection; implemented with next submission.

---
+++ Assets/Scripts/Kieran's Scripts/Test Versions/ParticleRotation.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleRotation : MonoBehaviour {
+
+    [SerializeField]
+    private float rotSpeed = 0.3f;
+
+	// Update is called once per frame
+	void Update () {
+
+        transform.eulerAngles += new Vector3(0, rotSpeed, 0);
+	}
+}
--- Assets/Scripts/Kieran's Scripts/Test Versions/Selected.cs
+++ Assets/Scripts/Kieran's Scripts/Test Versions/Selected.cs
@@ -1,51 +1,106 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Selected : MonoBehaviour {
 
-    public bool selected = false;
-    private bool selectedByClick = false;
-
     [SerializeField]
-    int counter = 0;
+    private bool selected = false;
+    [SerializeField]
+    private GameObject selectionGlow = null;
+    private GameObject glow = null;
+
+    private GameObject[] unitTypeArray;
+
+    //Selection Variables
+    bool selectAllEnabled = false;
+    bool callOnce = false;
 
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
+        DragClick();
+        ParticleSelection();
 
-        if (GetComponent<Renderer>().isVisible && Input.GetMouseButton(0))
+        if (Input.GetKeyDown(KeyCode.LeftAlt))
+            selectAllEnabled = !selectAllEnabled;
+
+        if (selectAllEnabled)
         {
-            if (!selectedByClick)
-            {
-                Vector3 camPos = Camera.main.WorldToScreenPoint(transform.position); //Transposes 3D Vector into 2D Screen Space (Unproject)
-                camPos.y = BoxSelector.ScreenToRectSpace(camPos.y);
-                selected = BoxSelector.selection.Contains(camPos);
-            }
-            
+            Debug.Log("Enabled");
+            SelectAllSpecificUnits();
+            callOnce = true;
+        }
+        else if (!selectAllEnabled && callOnce)
+        {
+            Debug.Log("Disabled");
+            DeselectAllSpecificUnits();
+            callOnce = false;
+        }
+          
+    }
+
+    /// <summary>
+    /// Manages the activation of particle effect system if unit is selected
+    /// </summary>
+    private void ParticleSelection()
+    {
+        if (selected && glow == null)
+        {
+            glow = (GameObject)GameObject.Instantiate(selectionGlow);    //Instance of the glow particle system is created & moved to the unit's location
+            glow.transform.parent = transform;
+            glow.transform.localPosition = new Vector3(0, -GetComponent<MeshFilter>().mesh.bounds.extents.y, 0);
+        }
+        else if (!selected && glow != null)
+        {
+            Destroy(glow);
+            glow = null;
         }
 
-        if (selected)
-            GetComponentInChildren<Projector>().enabled = true;
-        else
-            GetComponentInChildren<Projector>().enabled = false;
+    }
 
-        if (counter == 2)
+
+    public void DragClick()
+    {
+        if (Input.GetMouseButton(0))
         {
-            selected = false;
-            selectedByClick = false;
-            counter = 0;
+            Vector3 camPos = Camera.main.WorldToScreenPoint(transform.position); //Transposes 3D Vector into 2D Screen Space (Unproject)
+            camPos.y = BoxSelector.ScreenToRectSpace(camPos.y);
+            selected = BoxSelector.selection.Contains(camPos);
         }
     }
 
-    private void OnMouseDown()
+    public void OnMouseOver()
     {
-        counter++;
-        selected = true;
-        selectedByClick = true;
+
+        if(Input.GetMouseButton(0))
+        {
+            gameObject.GetComponent<Selected>().selected = true;
+        }
     }
 
-    private void OnMouseUp()
+
+    public void SelectAllSpecificUnits()
     {
-        if (selectedByClick)
-            selected = true;
+        unitTypeArray = GameObject.FindGameObjectsWithTag(gameObject.tag);
+        Debug.Log(gameObject.tag);
+        foreach (GameObject g in unitTypeArray)
+        {
+            g.GetComponent<Selected>().selected = true;
+        }
     }
+
+    public void DeselectAllSpecificUnits()
+    {
+        unitTypeArray = GameObject.FindGameObjectsWithTag(gameObject.tag);
+        foreach (GameObject g in unitTypeArray)
+        {
+            g.GetComponent<Selected>().selected = false;
+              g.GetComponent<Selected>().selected = false;
+           
+        }
+    }
+
+   
+  
 }
