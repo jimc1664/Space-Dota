0e239d5c08826827446ad0b83207cacad973c162
kbrawley95 <kieran.brawley95@gmail.com>
2016-03-31 16:35:21 +0100

    Mini Map (Re-Submit)

--- Assets/Scripts/Sys.cs
+++ Assets/Scripts/Sys.cs
@@ -109,7 +109,7 @@ public class Sys : MonoBehaviour {
         Started = true;
         GameUI.SetActive(true);
         StartUI.SetActive(false);
-       // minimap.SetActive(true);
+        minimap.SetActive(true);
         netHUD.enabled = false;
        
     }
